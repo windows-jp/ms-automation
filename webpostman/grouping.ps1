8d6b4803feff45c35c657f308fac0a5d0f09fcec
windows-jp <q5hzA8FK@gmail.com>
2022-05-21 11:05:09 +0900

    改行エスケープがなくてもShellにコピペできるので、改行エスケープは削除

--- webpostman/grouping.ps1
+++ webpostman/grouping.ps1
@@ -18,15 +18,15 @@ $group_identifier = Import-Csv $csv_file -Encoding default | Select-Object $send
 
 foreach ($gi in $group_identifier) {Import-Csv $csv_file -Encoding default | Where-Object {($_.$send_column -eq $gi.$send_column) -and ($_.$title_column -eq $gi.$title_column)} | Select-Object $sender_id, $recipient_email | Export-Csv -LiteralPath ($temp_dir + '\' +  $gi.$send_column + '_' + $gi.$title_column + '.csv').replace(' ', '_').replace('/','').replace(':','') -NoTypeInformation -Encoding default}
 
-foreach ($temp_csv in (Get-ChildItem $temp_dir)) { `
-    if (Test-Path $temp_csv.FullName) { `
-        foreach ($compared_csv in (Get-ChildItem $temp_dir -Exclude $temp_csv.Name)) { `
-                $difference_is_detected = Compare-Object (Get-Content -LiteralPath $temp_csv.FullName) (Get-Content -LiteralPath $compared_csv.FullName); `
-                if (-Not $difference_is_detected) { `
-                    Move-Item ($compared_csv.FullName) -Destination ($duplicate_dir) `
-                } `
+foreach ($temp_csv in (Get-ChildItem $temp_dir)) {
+    if (Test-Path $temp_csv.FullName) {
+        foreach ($compared_csv in (Get-ChildItem $temp_dir -Exclude $temp_csv.Name)) {
+                $difference_is_detected = Compare-Object (Get-Content -LiteralPath $temp_csv.FullName) (Get-Content -LiteralPath $compared_csv.FullName);
+                if (-Not $difference_is_detected) {
+                    Move-Item ($compared_csv.FullName) -Destination ($duplicate_dir)
+                }
         }
-    } `
+    }
 }
 
 foreach ($uniq_csv in (Get-ChildItem $temp_dir)){Get-Content -LiteralPath $uniq_csv.FullName | Add-Content ($work_dir + $out_dir + '/' + $out_file) }
