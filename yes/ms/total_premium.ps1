b3f0f7e86eeff3eb9d4533bc0c100d4279eeb264
windows-jp <q5hzA8FK@gmail.com>
2022-06-29 21:49:42 +0900

    MSのTOTAL_PREMIUM自動計算ツール作成完了

---
+++ yes/ms/total_premium.ps1
@@ -0,0 +1,16 @@
+﻿foreach ($file in Get-ChildItem C:\Users\opkd0\Desktop\MS -Recurse -Filter "MSFJ*.txt"){
+    $null = $file.Name -match '.*_(?<date>\d{8})_.*'
+    $date_str = [string]::Format("{0}/{1}/{2}", $matches.date.Substring(0,4), $matches.date.Substring(4,2), $matches.date.Substring(6,2))
+
+    $content = Get-Content $file.FullName
+
+    $total_premium = 0
+
+    foreach ($line in $content[1..($content.Length - 2)]){
+        $premium = [int]$line.Substring(728,4)
+        $total_premium += $premium
+    }
+
+    Write-Host "${date_str},${total_premium}"
+    
+}
